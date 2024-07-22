b26a5300700403d46d1611179fe8dd4333a60154
kwauso <akiranieda@keio.jp>
2024-07-22 14:37:00 +0900

    update

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,20 @@
 import express from 'express'
 const app: express.Express = express()
+
+const res_json = [
+    {id: 1, name: "Taro"},
+    {id: 2, name: "Hanako"},
+    {id: 3, name: "Keio"}
+]
+
 app.get("/", (req:express.Request, res:express.Response)=>{
     res.send("こんにちは");
 })
+
+app.get("/api",(req:express.Request, res:express.Response)=>{
+    res.json(res_json);
+});
+
 app.listen(3000,()=>{
     console.log('ポート3000番で起動しました。')
 })
