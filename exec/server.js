cb1161a1a1ac3e10b8b6733c1d1816902a3cf3c5
kwauso <akiranieda@keio.jp>
2024-07-27 09:38:06 +0000

    update

--- exec/server.js
+++ exec/server.js
@@ -4,10 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const userAPI_1 = __importDefault(require("./routes/userAPI"));
 const app = (0, express_1.default)();
-app.use(express_1.default.static('/home/akira/my_express_app/public'));
+app.use(express_1.default.static('/home/tokyo/my_express_app/public'));
 app.use('/api', userAPI_1.default);
+app.use((0, cors_1.default)());
 app.get("/", (req, res) => {
     res.send();
 });
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import api from './routes/userAPI';
 const app: express.Express = express();
 
-app.use(express.static('/home/akira/my_express_app/public'));
+app.use(express.static('/home/tokyo/my_express_app/public'));
 app.use('/api', api);
+app.use(cors());
 
 app.get("/", (req:express.Request, res:express.Response)=>{
     res.send();
