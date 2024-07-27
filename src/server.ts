40fba7882f02d43e9ccf8423bf53e106fae8c83a
kwauso <akiranieda@keio.jp>
2024-07-27 10:20:29 +0000

    update

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import cors from 'cors';
+const cors = require('cors');
 import api from './routes/userAPI';
 const app: express.Express = express();
 
 app.use(express.static('/home/tokyo/my_express_app/public'));
-app.use('/api', api);
 app.use(cors());
+app.use('/api', api);
+
 
 app.get("/", (req:express.Request, res:express.Response)=>{
     res.send();
